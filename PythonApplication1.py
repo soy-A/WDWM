3d09f2e16f4ba78f8e3c83445ae86a2a338e416a
nature1216 <63771579+nature1216@users.noreply.github.com>
2020-06-04 13:49:24 +0900

    Add files via upload
    중복되는 코드(제목, 게시날짜, 링크 출력) 함수로 묶고 최근공지 함수 수정

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -23,11 +23,21 @@ driver.get(url)
 #newpost : 새 글 아이콘 공지 출력
 #lastpost : 최신 공지 5개 출력
 #notice : 고정공지 출력
+#output : 공지 제목, 게시일, 링크 출력
 #변수
 #pagenum : 현재 페이지 숫자
 #postnum : 현재 페이지에서 게시글 순서
+#nextpagepost : 다음 페이지에서 출력해야 할 글 개수
 #순서
 #고정공지→새 글 아이콘 공지 or 최신공지 출력
+
+def output(postnum):
+    text = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).text
+    print("제목 :",text)
+    date = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[5]'%(postnum)).text
+    print("게시일 :",date)
+    link = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).get_attribute("href")
+    print(link)
 
 
 
@@ -38,17 +48,12 @@ def newpost(pagenum,postnum):
     th = driver.find_element_by_class_name('page_list').find_elements_by_tag_name('img')
     for i in th:
         img = i.get_attribute('alt')
-        if img=='새글' and postnum<len(th):
-            text = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).text
-            print("제목 :",text)
-            date = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[5]'%(postnum)).text
-            print("게시일 :",date)
-            link = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).get_attribute("href")
-            print(link)
-            print("postnum =",postnum)
-            print("pagenum =",pagenum)
+        if img=='새글' and postnum<len(th): 
+            output(postnum)
+            #print("postnum =",postnum)
+            #print("pagenum =",pagenum)
             postnum+=1
-        elif img=='새글' and postnum>9:
+        elif img=='새글' and postnum>9: # 페이지 넘어갔을때
             driver.find_element_by_xpath('//*[@id="print_area"]/div[2]/a[%d]'%(pagenum+1)).click()
             newpost(pagenum+1)
             postnum=0
@@ -58,41 +63,27 @@ def lastpost(pagenum,postnum):
     print("lastpost in")
     url="http://www.jbnu.ac.kr/kor/?menuID=452&pno={pagenum}"
     driver.get(url)
-    nextpagepost=5-postnum
-    print("nextpagenum =",nextpagepost)
-    if postnum<4:
+    nextpagepost=0
+    print("nextpagepost =",nextpagepost)
+    #페이지 안 넘어가고 5개를 뽑으려면 현재 postnum이 4 이하여야 함
+    if postnum>4: #페이지 넘어갔을 때
         for i in range(postnum+1,9):
-            text = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).text
-            print("제목 :",text)
-            date = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[5]'%(postnum)).text
-            print("게시일 :",date)
-            link = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).get_attribute("href")
-            print(link)
-            print("postnum =",postnum)
-            print("pagenum =",pagenum)
+            output(postnum)
+            #print("postnum =",postnum)
+            #print("pagenum =",pagenum)
             postnum+=1
         driver.find_element_by_xpath('//*[@id="print_area"]/div[2]/a[%d]'%(pagenum+1)).click()
         postnum=1
         for i in range(postnum,nextpagepost):
-            text = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).text
-            print("제목 :",text)
-            date = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[5]'%(postnum)).text
-            print("게시일 :",date)
-            link = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).get_attribute("href")
-            print(link)
-            print("postnum =",postnum)
-            print("pagenum =",pagenum)
+            output(postnum)
+            #print("postnum =",postnum)
+            #print("pagenum =",pagenum)
             postnum+=1
     else:
         for i in range(5):
-            text = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).text
-            print("제목 :",text)
-            date = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[5]'%(postnum)).text
-            print("게시일 :",date)
-            link = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).get_attribute("href")
-            print(link)
-            print("postnum =",postnum)
-            print("pagenum =",pagenum)
+            output(postnum)
+            #print("postnum =",postnum)
+            #print("pagenum =",pagenum)
             postnum+=1
 
 
@@ -105,14 +96,9 @@ def notice(pagenum):
     for i in th:
         notice = i.get_attribute('alt')
         if notice=='공지글' and postnum<len(th):
-            text = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).text
-            print("제목 :",text)
-            date = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[5]'%(postnum)).text
-            print("게시일 :",date)
-            link = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).get_attribute("href")
-            print(link)
-            print("postnum =",postnum)
-            print("pagenum =",pagenum)
+            output(postnum)
+            #print("postnum =",postnum)
+            #print("pagenum =",pagenum)
             postnum+=1
         elif notice=='공지글' and postnum>9:
             driver.find_element_by_xpath('//*[@id="print_area"]/div[2]/a[%d]'%(pagenum+1)).click()
