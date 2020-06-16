659651f418b052ff117c43b523fe96dcc54c4e54
nature1216 <63771579+nature1216@users.noreply.github.com>
2020-06-16 15:35:31 +0900

    Update corona.py
    json 코드 추가

--- corona.py
+++ corona.py
@@ -1,9 +1,11 @@
-
+import json
 from selenium import webdriver
 
 
-driver = webdriver.Chrome('C:/Users/김자연/Desktop/setups/chromedriver_win32/chromedriver.exe')
+driver = webdriver.Chrome('C:/chromedriver.exe')
 
+corona_dict = {}
+i = 0
 
 def output(postnum):
     text = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).text
@@ -12,6 +14,10 @@ def output(postnum):
     print("게시일 :",date)
     link = driver.find_element_by_xpath('//*[@id="print_area"]/div[1]/table/tbody/tr[%d]/td[2]/span/a'%(postnum)).get_attribute("href")
     print(link)
+    global corona_dict
+    global i
+    corona_dict[i] = {'text':text, 'date':date, 'link':link}    
+    i = i+1
 
 
 
@@ -33,10 +39,10 @@ def lastpost(pagenum,postnum):
     url="http://www.jbnu.ac.kr/kor/?menuID=452&pno={pagenum}"
     driver.get(url)
     nextpagepost=0
-    if postnum>4:
-        nextpagepost = 5-(9-postnum)
-        for i in range(postnum+1,10):
-            output(postnum+1)
+    if postnum>5:
+        nextpagepost = 5-(9-postnum-1)
+        for i in range(postnum,10):
+            output(postnum)
             postnum+=1
         driver.find_element_by_xpath('//*[@id="print_area"]/div[2]/a[%d]'%(pagenum+1)).click()
         postnum=0
@@ -45,8 +51,8 @@ def lastpost(pagenum,postnum):
             postnum+=1
     else:
         for i in range(5):
-            output(postnum+1)
-            postnum+=1
+            output(postnum+i)
+
 
 
 def notice(pagenum):
@@ -58,6 +64,14 @@ def notice(pagenum):
         notice = i.get_attribute('alt')
         if notice=='공지글' and postnum<len(th):
             output(postnum)
+            postnum+=1
     newpost(pagenum,postnum)
 
+def toJson(corona_dict):
+    with open('corona.json','w',encoding='utf-8') as file:
+        json.dump( corona_dict,file,ensure_ascii=False,indent='\t')
+
+
+
 notice(1)
+toJson(corona_dict)
