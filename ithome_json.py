ab34e48236d0ea462fe2cc88d1cbf278a96720fc
soy-A <63772786+soy-A@users.noreply.github.com>
2020-06-29 17:47:00 +0900

    Update ithome_json.py
    json파일 작성 방식 수정

--- ithome_json.py
+++ ithome_json.py
@@ -9,25 +9,21 @@ options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(executable_path = '/workspace/chromedriver', options = options)
 
-from collections import OrderedDict
-
-file_data = OrderedDict()
+it_dict = []
 
 def notice():
 	driver.get('https://it.jbnu.ac.kr/it/index.do')
 
-	data = {}
 	for i in range(5):
 		text = driver.find_element_by_xpath('//*[@id="recentBbsArtclObj_681_1050"]/li[%d]/a'%(i+1)).text
 		title = text[:-6]
 		date = text[-5:]
 		url = driver.find_element_by_xpath('//*[@id="recentBbsArtclObj_681_1050"]/li[%d]/a'%(i+1)).get_attribute("href")
-		file_data["title"] = title
-		file_data["date"] = date
-		file_data["url"] = url
-		data[i]=file_data
-	with open('ithome.json', 'w', encoding = "utf-8") as make_file:
-		json.dump(data, make_file, ensure_ascii=False, indent="\t")
+		it_dict.append({'title':title, 'date':date, 'url':url})
+
+def toJson(it_json):
+    with open('ithome.json','w',encoding='utf-8') as file:
+        json.dump( it_json,file,ensure_ascii=False,indent='\t')
 
 class Repeat:
 
@@ -36,6 +32,7 @@ class Repeat:
 
 	def Reithome(self):
 		notice()
+		toJson(it_dict)
 		threading.Timer(3600,self.Reithome).start()
 
 re = Repeat()
